5d276f2314a416ed93e4517b1634f71f220e7ea3
Gilson <apcturvo@gmail.com>
2026-03-16 10:43:04 -0300

    AtualizaÃ§Ã£o

--- atualizar.sh
+++ atualizar.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-# atualizar.sh
+# atualizar.sh - Localizado na raiz do site
+
+echo "--- Entrando na pasta do Git ---"
+cd public_html
+
 echo "--- Baixando novos arquivos do GitHub ---"
 git pull origin main
 
 echo "--- Sincronizando arquivos com o site ---"
-# Copia o tema
-cp -r public_html/wp-content/themes/fitness-club/* wp-content/themes/fitness-club/
+# Copia o tema para a pasta ativa (subindo um nível)
+cp -r wp-content/themes/fitness-club/* ../wp-content/themes/fitness-club/
 
-# Garante a pasta de uploads e a imagem
-mkdir -p wp-content/uploads/espaco-aluno/
-cp public_html/wp-content/uploads/espaco-aluno/academia-bg.png wp-content/uploads/espaco-aluno/
+# Garante a pasta de uploads e a imagem na raiz do site
+mkdir -p ../wp-content/uploads/espaco-aluno/
+cp wp-content/uploads/espaco-aluno/academia-bg.png ../wp-content/uploads/espaco-aluno/
 
 echo "--- TUDO PRONTO! Verifique o site. ---"
